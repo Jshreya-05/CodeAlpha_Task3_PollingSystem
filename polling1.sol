3691d549373f15fc058c07fcc8bd9d0234fc649a
Jshreya-05 <jshreya499@gmail.com>
2026-04-27 19:53:04 +0530

    Update polling1.sol

--- polling1.sol
+++ polling1.sol
@@ -6,6 +6,7 @@ contract Polling {
     string public question;
     string[] public options;
     uint public endTime;
+    bool public pollExists;
 
     mapping(uint => uint) public votes;
     mapping(address => bool) public hasVoted;
@@ -16,13 +17,17 @@ contract Polling {
         string[] memory _options,
         uint _duration
     ) public {
+        require(!pollExists, "Poll already exists");
+
         question = _question;
         options = _options;
         endTime = block.timestamp + _duration;
+        pollExists = true;
     }
 
-    // Vote for an option
+    // Vote
     function vote(uint optionIndex) public {
+        require(pollExists, "No poll created");
         require(block.timestamp < endTime, "Poll ended");
         require(!hasVoted[msg.sender], "Already voted");
         require(optionIndex < options.length, "Invalid option");
@@ -31,7 +36,7 @@ contract Polling {
         hasVoted[msg.sender] = true;
     }
 
-    // Get votes of an option
+    // Get votes
     function getVotes(uint optionIndex) public view returns (uint) {
         return votes[optionIndex];
     }
@@ -39,5 +44,22 @@ contract Polling {
     // Get option name
     function getOption(uint index) public view returns (string memory) {
         return options[index];
+    }
+
+    // Get winner
+    function getWinner() public view returns (string memory) {
+        require(block.timestamp >= endTime, "Poll still active");
+
+        uint maxVotes = 0;
+        uint winnerIndex = 0;
+
+        for (uint i = 0; i < options.length; i++) {
+            if (votes[i] > maxVotes) {
+                maxVotes = votes[i];
+                winnerIndex = i;
+            }
+        }
+
+        return options[winnerIndex];
     }
 }
